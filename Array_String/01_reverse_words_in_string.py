3ca3e758fdc0fa6cfb73b81dad7f37e9d2a1e03d
Piyush <alveusdweller@gmail.com>
2024-03-13 06:15:46 +0530

    Updated solution to reverse words in a string

--- Array_String/01_reverse_words_in_string.py
+++ Array_String/01_reverse_words_in_string.py
@@ -4,6 +4,7 @@
 # "     t  adsfn adfbscns     adsf nsmf    a dfasdnfjams   adjajlsfhjks f as da da d a    "
 
 
+# Approach 1: swapping words and restitching the string
 def reverseWords(self, s: str) -> str:
     s = s.lstrip(" ")
     s = s.rstrip(" ")
@@ -47,3 +48,96 @@ def reverseWords(self, s: str) -> str:
         # print(idx, s[idx], jdx, s[jdx])
 
     return s
+
+
+# Approach 2: Bringing each word to the front
+# Wouldn't work in python, try in cpp where string is mutable and can be updated in place.
+def _reverse(self, s: str, start: int, end: int) -> str:
+    while start < end:
+        s[start], s[end] = s[end], s[start]
+        start += 1
+        end -= 1
+
+    return s
+
+
+def reverseWords(self, s: str) -> str:
+    n = len(s)
+    # Reverse the entire string
+    s = self._reverse(s, 0, n - 1)
+
+    # Reverse every word.
+    k = 0
+    while k < n:
+        # reverse part of the string
+        while k < n and s[k] == " ":
+            k += 1
+        start = k
+        while k < n and s[k] != " ":
+            k += 1
+        end = k - 1
+        s = self._reverse(s, start, end)
+
+    # Trim off extra spaces between words
+    idx, jdx = 0, 0
+    while jdx < n:
+        # moving the words forward
+        while jdx < n and s[jdx] == " ":
+            jdx += 1
+        while jdx < n and s[jdx] != " ":
+            s[idx] = s[jdx]
+            idx += 1
+            jdx += 1
+        s[idx] = " "
+        idx += 1
+
+    s = s.rstrip(" ")
+    return s
+
+
+# string reverse(string s, int start, int end) {
+#     while (start < end){
+#         char temp = s[start];
+#         s[start] = s[end];
+#         s[end] = temp;
+#         start++;
+#         end--;
+#     }
+#     return s;
+# }
+
+# string reverseWords(string s) {
+#     int n = s.length();
+#     // Reverse the entire string
+#     s = reverse(s, 0, n - 1);
+
+#     // Reverse every word
+#     int k = 0;
+#     while (k < n) {
+#         // reverse part of the string
+#         while (k<n && isspace(s[k])) k += 1;
+#         int start = k;
+#         while (k<n && !isspace(s[k])) k += 1;
+#         int end = k - 1;
+#         s = reverse(s, start, end);
+#     }
+
+#     // Trim off extra spaces between words
+#     int idx=0,jdx=0;
+#     while (jdx < n) {
+#         while (jdx<n and isspace(s[jdx])) jdx++;
+#         while (jdx<n and !isspace(s[jdx])) {
+#             s[idx] = s[jdx];
+#             idx++; jdx++;
+#         }
+#         while (jdx<n and isspace(s[jdx])) jdx++;
+
+#         if (jdx<n) {
+#             s[idx] = ' ';
+#             idx++;
+#         }
+#     }
+
+#     s = s.substr(0, idx);
+#     return s;
+# }
